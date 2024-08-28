873928fe959404d1412d813084a438e512967a64
jacopolegovini <legovini96@gmail.com>
2024-08-28 14:39:48 +0200

    milestone 0 completed

---
+++ script.js
@@ -0,0 +1,11 @@
+const ourTeam = [
+    { firstName: 'Wayne Barnett', role: 'Founder & CEO', photo: 'wayne-barnett-founder-ceo.jpg'},
+    { firstName: 'Angela Caroll', role: 'Chief Editor', photo: 'angela-caroll-chief-editor.jpg'},
+    { firstName: 'Walter Gordon', role: 'Office Manager', photo: 'walter-gordon-office-manager.jpg'},
+    { firstName: 'Angela Lopez', role: 'Social Media Manager', photo: 'angela-lopez-social-media-manager.jpg'},
+    { firstName: 'Scott Estrada', role: 'Developer', photo: 'scott-estrada-developer.jpg'},
+    { firstName: 'Barbara Ramos', role: 'Graphic Designer', photo: 'barbara-ramos-graphic-designer.jpg'}
+]
+
+console.log(ourTeam)
+console.table(ourTeam)
